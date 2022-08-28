2c9f12101b867200d36e7170de3fe53eaa8b196c
Dann Teng-Lee Dempsey <dann.dempsey@booking.com>
2022-08-28 22:48:41 +0200

    Allow partial last signature.

--- paginator.py
+++ paginator.py
@@ -3,10 +3,14 @@ from wand.image import Image
 
 def signature_page_order(num_up, pages_per_signature):
     if num_up == 4:
-        return [5, 12, 4, 13,
-                11, 6, 14, 3,
-                7, 10, 2, 15,
-                9, 8, 16, 1]
+        if pages_per_signature == 16:
+            return [5, 12, 4, 13,
+                    11, 6, 14, 3,
+                    7, 10, 2, 15,
+                    9, 8, 16, 1]
+        if pages_per_signature == 8:
+            return [3, 6, 2, 7,
+                    5, 4, 8, 1]
     if num_up == 2:
         if pages_per_signature == 12:
             return [12, 1, 2, 11,
@@ -23,19 +27,28 @@ def signature_page_order(num_up, pages_per_signature):
 
 
 def should_rotate_page(page_num, num_up, pages_per_signature):
-    return num_up == 4 and pages_per_signature == 16 and page_num in [5, 12, 11, 6, 7, 10, 9, 8]
+    if num_up == 4:
+        if pages_per_signature == 16:
+            return page_num in [5, 12, 11, 6, 7, 10, 9, 8]
+        if pages_per_signature == 8:
+            return page_num in [3, 6, 5, 4]
+    return False
 
 
 def write_paginated_images(image: Image, n_up: int, pages_per_signature: int, filename: str):
     if len(image.sequence) % pages_per_signature != 0:
         n = pages_per_signature - len(image.sequence) % pages_per_signature
         p = image.sequence[0]
-        print(f"adding {n} blank pages to end")
+        n %= n_up * 2
+        print(f"adding {n} blank pages to end. (There are now "
+              f"{(len(image.sequence) + n) % pages_per_signature} pages in the last signature.")
         for x in range(n):
             image.sequence.append(Image(height=p.height, width=p.width, resolution=p.resolution))
     paginated_image = Image()
     num_signatures = int(len(image.sequence) / pages_per_signature)
     for n in range(num_signatures):
+        if len(image.sequence) - n * pages_per_signature < pages_per_signature:
+            pages_per_signature = len(image.sequence) - n * pages_per_signature
         for i in signature_page_order(n_up, pages_per_signature):
             big_i = pages_per_signature * n + i - 1
             page = image.sequence[big_i]
