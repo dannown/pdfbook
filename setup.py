3a0546d1069cc40dab26bf2051b32db837d597c2
Dann Teng-Lee Dempsey <dann.dempsey@booking.com>
2022-08-30 12:24:00 +0200

    Move books into package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pdfbook',
+    version='1',
+    packages=[''],
+    url='',
+    license='',
+    author='dann dempsey',
+    author_email='dann@dann.org',
+    description=''
+)
